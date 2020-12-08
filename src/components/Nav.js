7dde64c7a1e27b94bb510e32c01def82eb3743c6
wehearyoudeveloper <wehearyou.developer@gmail.com>
2020-12-07 19:57:54 -0500

    More file setup. ThemeProvider works

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { Box } from "rebass"
+
+const Nav = props => {
+  return (
+    <>
+      <Box width="100%" height="100px" {...props} />
+    </>
+  )
+}
+
+export default Nav
---
+++ src/theme.js
@@ -0,0 +1,42 @@
+export default {
+    breakpoints: ['40em', '52em', '64em'],
+    fontSizes: [
+      12, 14, 16, 20, 24, 32, 48, 64
+    ],
+    colors: {
+      orange: '#F3784A',
+      blue: '#131B41',
+      yellow: '#FDBE4E'
+    },
+    gradients: {
+      heroOrange: `linear-gradient(180deg, yellowWHY 0%, orangeWHY 100%)`,
+    },
+    space: [
+      '0rem', '4rem', '8rem', '16rem', '32rem', '64rem', '128rem', '256rem'
+    ],
+    fonts: {
+      body: 'roboto, sans-serif',
+      monospace: 'Menlo, monospace',
+    },
+    fontWeights: {
+      body: 400,
+      heading: 900,
+      bold: 700,
+    },
+    lineHeights: {
+      body: 1.5,
+      heading: 1.25,
+    },
+    shadows: {
+      small: '0 0 4px rgba(0, 0, 0, .125)',
+      large: '0 0 24px rgba(0, 0, 0, .125)'
+    },
+    variants: {
+    },
+    buttons: {
+      primary: {
+        color: 'white',
+        bg: 'primary',
+      }
+    }
+  }
